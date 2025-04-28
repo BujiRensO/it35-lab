9efcd1b3c3edae89f02b6cab9ac2252a92075d20
BujiRensO <codin.abarquez@gmail.com>
2025-04-28 15:02:35 +0800

    fix feed

--- src/pages/home-tabs/Feed.tsx
+++ src/pages/home-tabs/Feed.tsx
@@ -24,16 +24,6 @@ const Feed: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
-        <div
-          style={{
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'center',
-            height: '100%',
-          }}
-        >
-          Feed
-        </div>
         <FeedContainer />
       </IonContent>
     </IonPage>
