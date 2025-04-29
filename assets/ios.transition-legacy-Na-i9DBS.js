638afa96507570d455e97e018c2018a5abaf7dea
BujiRensO <codin.abarquez@gmail.com>
2025-04-29 22:14:58 +0800

    Updates

--- assets/ios.transition-legacy-Na-i9DBS.js
+++ assets/ios.transition-legacy-Na-i9DBS.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BdrAITUd.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-CxDt7Biv.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-TIy9e79K.js
+++ assets/swipe-back-legacy-TIy9e79K.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-BdrAITUd.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-CxDt7Biv.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
